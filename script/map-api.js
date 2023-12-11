7e88dded249915b509434e14a3a96089b982987e
bon-ie <145976573+bon-ie@users.noreply.github.com>
2023-12-11 18:57:00 +1100

    Add files via upload

--- script/map-api.js
+++ script/map-api.js
@@ -1,10 +1,10 @@
 // 1. Initialise the map 
-function initializeMap() {
+function GetMap() {
   const map = new Microsoft.Maps.Map(document.querySelector("#map-location"), {
     credentials:
       "ArWU34cz1I4S_ysnN1G5A3yJJp1NPk4CObkmnW0sQPu5R5keJArPQYTVnKkCnL0y",
     center: new Microsoft.Maps.Location(-37.812528, 144.973335),
-    zoom: 14,
+    zoom: 15,
   });
 
   // 2. Add a pin at location
@@ -21,8 +21,9 @@ function initializeMap() {
 function loadMapScript() {
   const script = document.createElement("script");
   script.src =
-    "https://www.bing.com/api/maps/mapcontrol?key=YOUR_API_KEY&callback=initializeMap";
+    "http://www.bing.com/api/maps/mapcontrol?callback=GetMap&key=ArWU34cz1I4S_ysnN1G5A3yJJp1NPk4CObkmnW0sQPu5R5keJArPQYTVnKkCnL0y";
   document.head.appendChild(script);
 }
 
 document.addEventListener("DOMContentLoaded", loadMapScript);
+
